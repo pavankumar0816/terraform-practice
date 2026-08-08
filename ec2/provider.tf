ab560ba463c0399dc757440dca13907cf1ade325
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-08-08 16:23:46 +0530

    terraform

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -10,3 +10,7 @@ terraform {
 provider "aws" { # tells terraform how to configure that provider (This is about runtime configuration)
     region = "us-east-1"
 }
+
+# provider "aws" is used to configure the AWS provider. If the region is not provided through another supported configuration method,
+# we specify the region inside the provider block so Terraform knows where to create/manage AWS resources.
+
